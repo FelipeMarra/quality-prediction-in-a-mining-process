f5675d6a9bf0e91f9a6f2d408fabc17dea8a536d
Felipe Marra <felipeferreiramarra@aluno.ufsj.edu.br>
2023-08-23 10:12:10 -0300

    load raw data

--- src/data_processing/processed_dataset.py
+++ src/data_processing/processed_dataset.py
@@ -1,25 +1,57 @@
-# -*- coding: utf-8 -*-
+# %%
 import os
 import pandas as pd
 import string
 import numpy as np
 
+# %%
+# Load raw data
 
 def load_raw_data(path: string) -> pd.DataFrame:
     """
     Load csv data using pandas and return a dataframe
     """
 
-    df = pd.read_csv(path, sep=",", decimal=".", index_col='Data',
-                     parse_dates=['Data'])
+    df = pd.read_csv(path, sep=",", decimal=",", index_col='date',
+                     parse_dates=['date'])
 
     return df
 
-
-# --------------------------------------------
-# Load data
-path = r"/Users/<your_user>/xxx/xxx/data/"
-final_path = os.path.join(path, "xxxx.csv")
-
+path =  os.path.normpath(os.getcwd() + os.sep + os.pardir + os.sep + os.pardir + "/data")
+final_path = os.path.join(path, "raw/MiningProcess_Flotation_Plant_Database.csv")
 df_raw = load_raw_data(path=final_path)
-df_raw = df_raw.sort_index()
+
+
+# %%
+df_raw.head(5)
+
+# %%
+df_raw.tail(5)
+
+# There are date in the same date and hor becouse some columns were sampled every 20 seconds.
+# Others were sampled on a hourly base.
+
+# The second (% Iron Feed) and third (% Silica Feed) columns are quality measures of the iron ore
+# pulp right before it is fed into the flotation plant. 
+
+# Column 4 until column 8 (Starch Flow,	Amina Flow, Ore Pulp Flow, Ore Pulp pH, Ore Pulp Density)
+# are the most important variables that impact in the ore quality in the end of the process.
+
+# From column 9 until column 22, we can see process data (level and air flow inside the flotation 
+# columns), which also impact in ore quality.
+
+# The last two columns (% Iron Concentrate,	% Silica Concentrate) are the final iron ore pulp 
+# quality measurement from the lab.
+
+#Target is to predict the last column, which is the % of silica in the iron ore concentrate.
+
+# Q. Is it possible to predict % Silica Concentrate every minute?
+# Q. How many steps (hours) ahead can we predict % Silica in Concentrate? This would help engineers
+# to act in predictive and optimized way, mitigatin the % of iron that could have gone to 
+# tailings.
+# Q. Is it possible to predict % Silica in Concentrate whitout using % Iron Concentrate column
+# (as they are highly correlated)?
+
+# %%
+df_raw.describe()
+# %%
