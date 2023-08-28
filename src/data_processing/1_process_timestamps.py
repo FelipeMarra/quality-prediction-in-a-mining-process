8d8d08bc4d2e90d51d132cc9fd2366ace4c6abd5
Felipe Marra <felipeferreiramarra@aluno.ufsj.edu.br>
2023-08-28 14:13:22 -0300

    convertion to 20 in 20s looks right

--- src/data_processing/1_process_timestamps.py
+++ src/data_processing/1_process_timestamps.py
@@ -16,34 +16,24 @@ df_raw.head()
 # %%
 # Convert to the 20 in 20s freq
 # asfreq wont work in a duplicated index
+#skip the 1st as it will ramin with 0 seconds
 indexes = df_raw.index.to_series()
 
 hr_counter = 0
 g_counter = 0 #global counter
 last_idx = indexes[0]
 
-for current_index in indexes:
+for current_idx in indexes:
     # For each hour (indexes are repeated hourly)
-    if(last_idx == current_index):
-        # Skip first
-        if(hr_counter == 0):
-            continue
-
+    if(last_idx == current_idx):
         # Add 20s * counter (20 in 20s)
         indexes[g_counter] = indexes[g_counter] + pd.to_timedelta(20 * hr_counter, unit='s')
         hr_counter = hr_counter + 1
     else:
-        # The iteration when the index change will already skip
-        # the first, so counter will restart with 1
-        hr_counter = 1
-        last_idx = current_index
+        hr_counter = 0
+        last_idx = current_idx
 
     g_counter = g_counter + 1
-
-# %%
-# TODO: In 9/9 at 23 hrs it begins at 23:58:00
-# and at 22hrs it ends at 22:57:40
-indexes[-183:-1]
 
 # %%
 # Drop datetimes past 2017-03-29 11:00:00 to remove the part without data
